699218997503916b986ec7c0c80057573ac0aaf5
joeyshi12 <46363213+joeyshi12@users.noreply.github.com>
2019-12-06 22:24:52 -0800

    Implemented Button class and Page class

---
+++ src/main/model/Button.py
@@ -0,0 +1,71 @@
+import pygame
+from pygame import Surface, Rect
+
+
+class Button:
+    def __init__(self, identifier: str, pos: tuple, dimensions: tuple, command=None, text=None, font=None):
+        self.identifier = identifier
+        self.button_rect = pygame.Rect(pos, dimensions)
+        self.command = command
+        self.text = text
+        self.font = font
+
+    def execute(self):
+        """executes self.command()"""
+        self.command()
+
+    def render_text(self) -> (Surface, Rect):
+        """returns a rendered text with proper font in black colour and
+a rectangle in which the rendered text will be placed in"""
+        text_surface = self.font.render(self.text, True, (0, 0, 0))
+        return text_surface, text_surface.get_rect()
+
+    def draw_text(self, display: Surface):
+        """draws rendered text in the center of rectangle"""
+        text_surf, text_rect = self.render_text()
+        center_x = (self.button_rect.left + (self.button_rect.width / 2))
+        center_y = (self.button_rect.top + (self.button_rect.height / 2))
+        text_rect.center = (center_x, center_y)
+        display.blit(text_surf, text_rect)
+
+    def draw_state(self, display: Surface, state: str):
+        """draws rectangle with a colour depending on the given state"""
+        if state == 'regular':
+            pygame.draw.rect(display, (255, 0, 0), self.button_rect)
+        elif state == 'within':
+            pygame.draw.rect(display, (0, 255, 0), self.button_rect)
+        elif state == 'clicking':
+            pygame.draw.rect(display, (0, 0, 255), self.button_rect)
+
+    def is_within(self, mouse_pos: tuple) -> bool:
+        """returns true if mouse is inside rectangle; false otherwise"""
+        is_within_x = (self.button_rect.left < mouse_pos[0] < self.button_rect.left + self.button_rect.width)
+        is_within_y = (self.button_rect.top < mouse_pos[1] < self.button_rect.top + self.button_rect.height)
+        return is_within_x and is_within_y
+
+    def draw_button(self, display: Surface, mouse_pos: tuple, clicking: bool):
+        """execute draw_text on display if text exists and draw_state on display
+with state given by:
+
+is_within and clicking     -> 'clicking'
+is_within and not clicking -> 'within'
+not is_within              -> 'regular'"""
+        if (self.text is not None) and (self.font is not None):
+            if self.is_within(mouse_pos):
+                if clicking:
+                    self.draw_state(display, 'clicking')
+                    self.draw_text(display)
+                else:
+                    self.draw_state(display, 'within')
+                    self.draw_text(display)
+            else:
+                self.draw_state(display, 'regular')
+                self.draw_text(display)
+        else:
+            if self.is_within(mouse_pos):
+                if clicking:
+                    self.draw_state(display, 'clicking')
+                else:
+                    self.draw_state(display, 'within')
+            else:
+                self.draw_state(display, 'regular')
--- src/main/ui/Main.py
+++ src/main/ui/Main.py
@@ -1,47 +1,52 @@
 import pygame
 
 from src.main.model.Block import *
+from src.main.model.Button import Button
+from src.main.model.Page import Page
+from src.main.ui.KeyHandling import key_handle_down, key_handle_up
 
 pygame.init()
 pygame.display.set_caption('Blocky World Program')
 clock = pygame.time.Clock()
 
-width = 500
-height = 500
+width = 600
+height = 400
 block_width = 80
 block_height = 60
 fps = 100
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
-white = (255, 255, 255)
 black = (0, 0, 0)
-
-block = Block(width / 2, height / 2, 0, 0, 50, 75, black)
-
-
-def key_handle_down(b: Block, key: str):
-    """updates the velocity of the block based on the key pressed down"""
-    if key == pygame.K_w:
-        b.set_vy(-6)
-    elif key == pygame.K_s:
-        b.set_vy(6)
-    elif key == pygame.K_a:
-        b.set_vx(-6)
-    elif key == pygame.K_d:
-        b.set_vx(6)
+white = (255, 255, 255)
+block = Block(width / 2 - block_width / 2, height / 2 - block_height / 2, 0, 0, block_height, block_width, black)
 
 
-def key_handle_up(b: Block, key: str):
-    """returns velocity to 0 when keys corresponding to a direction is let go"""
-    if key == pygame.K_w or key == pygame.K_s:
-        b.set_vy(0)
-    elif key == pygame.K_a or key == pygame.K_d:
-        b.set_vx(0)
+def menu_page():
+    display = pygame.display.set_mode((width, height))
+    start_button = Button("start_button", (0, 0), (100, 40), main_page, "Start",
+                          pygame.font.Font("freesansbold.ttf", 12))
+    settings_button = Button("settings_button", (0, 0), (100, 40), main_page, "Settings",
+                             pygame.font.Font("freesansbold.ttf", 12))
+    button_list = [start_button, settings_button]
+    page = Page(display, button_list)
+    page.arrange_buttons("vertical", (width / 2 - 50, height / 2 - 20), 100)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                page.on_click(pygame.mouse.get_pos())
+
+        display.fill(white)
+        page.draw_buttons(display, pygame.mouse.get_pos(), pygame.mouse.get_pressed())
+        pygame.display.update()
+        clock.tick(200)
 
 
-if __name__ == '__main__':
-    game_display = pygame.display.set_mode((width, height))
+def main_page():
+    display = pygame.display.set_mode((width, height))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -52,9 +57,13 @@ if __name__ == '__main__':
             if event.type == pygame.KEYUP:
                 key_handle_up(block, event.key)  # key handling
 
-        game_display.fill(white)
+        display.fill(white)
         block.update_block()  # update block
-        pygame.draw.rect(game_display, block.colour,
+        pygame.draw.rect(display, block.colour,
                          (block.x, block.y, block.width, block.height), 0)  # render block
         pygame.display.update()
         clock.tick(fps)
+
+
+if __name__ == '__main__':
+    menu_page()
